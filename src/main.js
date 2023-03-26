156faaa65025e2ba7a399ceeca28ff2b7d16b4a5
Daniil Zavyalov <dezavv1@gmail.com>
2023-03-26 21:30:21 +0600

    Open file dialog

--- src/main.js
+++ src/main.js
@@ -1,28 +1,66 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const electronApp = require('electron').app;
+const electronBrowserWindow = require('electron').BrowserWindow;
+const electronDialog = require('electron').dialog;
+const electronIpcMain = require('electron').ipcMain;
 
-const loadMainWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width : 1200,
-        height: 800,
+const nodePath = require("path");
+
+function createWindow() {
+    const window = new electronBrowserWindow({
+        x: 0,
+        y: 0,
+        width: 800,
+        height: 600,
+        show: false,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: nodePath.join(__dirname, 'preload.js')
         }
     });
 
-    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    window.loadFile('src/index.html')
+        .then(() => { window.show(); });
+
+    return window;
 }
 
-app.on("ready", loadMainWindow);
+electronApp.on('ready', () => {
+    window = createWindow();
+});
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
+electronApp.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        electronApp.quit();
     }
 });
 
-app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        loadMainWindow();
+electronApp.on('activate', () => {
+    if (electronBrowserWindow.getAllWindows().length === 0) {
+        createWindow();
     }
 });
+
+// ---
+
+// Only pass in a valid defaultPath
+let defaultPath = '/home/danzav'; // Ubuntu
+// let defaultPath = 'C:\\Users\\user\\invalid\\path'; // Windows
+
+electronIpcMain.handle('openFileDialog', () => {
+    // Dialog options
+    let options = {
+        title: 'Browse mapped drive',
+        defaultPath: defaultPath,
+        properties: ['openFile', 'multiSelections']
+    };
+
+    // Open dialog
+    return electronDialog.showOpenDialog(window, options)
+        .then((result) => {
+            // Bail early if user cancelled dialog
+            if (result.canceled) { return }
+
+            return result.filePaths;
+        })
+})
